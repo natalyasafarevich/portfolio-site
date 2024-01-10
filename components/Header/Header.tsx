606399b8cca27506832a1565e967c7d69f290b80
Natalya <natalyasafarevich@gmail.com>
2024-01-10 19:48:20 +0100

    header are added

---
+++ components/Header/Header.tsx
@@ -0,0 +1,40 @@
+'use client';
+import {FC, useEffect, useState} from 'react';
+import './Header.scss';
+import ScrollIntoView from 'react-scroll-into-view';
+import {HEADER_LINKS} from '@/constants/constants';
+
+const Header: FC = () => {
+  const [activeLink, setActiveLink] = useState('');
+
+  const handleLinkClick = (id: any) => {
+    setActiveLink(id);
+  };
+  return (
+    <header className='header'>
+      <div className='header__wrap'>
+        <nav className='header__nav'>
+          <ul className='header__list'>
+            {HEADER_LINKS.map((item, i) => {
+              let title = item.title.toLocaleLowerCase();
+              return (
+                <li key={i}>
+                  <ScrollIntoView
+                    smooth={true}
+                    selector={`#${title}`}
+                    onClick={() => handleLinkClick(title)}
+                    className={activeLink === title ? 'active' : ''}
+                  >
+                    <a href={`#${title}`}>{item.title}</a>
+                  </ScrollIntoView>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ constants/constants.ts
@@ -0,0 +1,23 @@
+type HeaderLinksProps = {
+  title: string;
+};
+export const HEADER_LINKS: Array<HeaderLinksProps> = [
+  {
+    title: 'Home',
+  },
+  {
+    title: 'About',
+  },
+  {
+    title: 'Service',
+  },
+  {
+    title: 'Resume',
+  },
+  {
+    title: 'Project',
+  },
+  {
+    title: 'Contact',
+  },
+];
