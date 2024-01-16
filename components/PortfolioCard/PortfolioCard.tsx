800675b8684d5a1506b0b5aa387ac4b2fc1bb634
Natalya <natalyasafarevich@gmail.com>
2024-01-16 15:52:34 +0100

    adaptive for portfolio has been added

--- components/PortfolioCard/PortfolioCard.tsx
+++ components/PortfolioCard/PortfolioCard.tsx
@@ -1,4 +1,4 @@
-import {FC, useState} from 'react';
+import {FC, useEffect, useState} from 'react';
 import './PortfolioCard.scss';
 import {CardPProps} from '@/constants/constants';
 import {Link} from 'react-scroll';
@@ -9,36 +9,35 @@ type PortfolioCardProps = {
 };
 
 const PortfolioCard: FC<PortfolioCardProps> = ({card}) => {
-  // const [ref, inView] = useInView({
-  //   triggerOnce: true,
-  //   threshold: 0.1,
-  // });
-
   const [isShown, setIsShown] = useState(false);
 
+  useEffect(() => {
+    console.log(isShown);
+  }, [isShown]);
+
   return (
-    <div>
-      <Link
-        to='/'
-        className={`portfolio-card ${isShown ? 'show' : ''}`}
-        onMouseEnter={() => setIsShown(true)}
-        onMouseLeave={() => setIsShown(false)}
-        // Добавляем ref здесь
+    <Link
+      to='/пквпк'
+      className={`portfolio-card ${isShown ? 'show' : ''}`}
+      onMouseEnter={() => {
+        setIsShown(true);
+      }}
+      onMouseLeave={() => setIsShown(false)}
+      // Добавляем ref здесь
+    >
+      <span
+        className={`portfolio-card__wrap `}
+        style={{
+          background: `center/cover no-repeat url(${card.src})`,
+        }}
       >
-        <span
-          className={`portfolio-card__wrap `}
-          style={{
-            background: `center/cover no-repeat url(${card.src})`,
-          }}
-        >
-          <span className='portfolio-card__button'></span>
-          <span className={`portfolio-card__info `}>
-            <span className={`portfolio-card__title`}>{card.title}</span>
-            <span className={`portfolio-card__desc`}>{card.desc}</span>
-          </span>
+        <span className='portfolio-card__button'></span>
+        <span className={`portfolio-card__info `}>
+          <span className={`portfolio-card__title`}>{card.title}</span>
+          <span className={`portfolio-card__desc`}>{card.desc}</span>
         </span>
-      </Link>
-    </div>
+      </span>
+    </Link>
   );
 };
 
